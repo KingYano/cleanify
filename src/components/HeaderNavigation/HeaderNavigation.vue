f1976cb2eab3a11477954e9109c4506cc30d379a
KingYano <belkadi.yanis.l@gmail.com>
2024-03-14 10:59:16 +0100

    ajout premier composant et modification base.css

---
+++ src/components/HeaderNavigation/HeaderNavigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header-nav">
+    <h1 class="header-nav-title">Cleanify</h1>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+  @import 'HeaderNavigation';
+</style>
