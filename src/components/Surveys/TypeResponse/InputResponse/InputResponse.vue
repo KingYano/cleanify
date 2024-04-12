8bd8a857a595a0a0720c1bf58b5522bde48f5d17
KingYano <belkadi.yanis.l@gmail.com>
2024-04-12 11:50:07 +0200

    ajout des nombres uniquement

--- src/components/Surveys/TypeResponse/InputResponse/InputResponse.vue
+++ src/components/Surveys/TypeResponse/InputResponse/InputResponse.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="input-response" >
     <label class="input-response-label" for="name"></label>
-    <input class="input-response-input" type="text" name="name" id="name" placeholder="name" required v-model="inputValue" @input="updateInput">
+    <input class="input-response-input" type="text" name="name" id="name" placeholder="name" required v-model.number="inputValue" @input="updateInput">
+    <p v-if="errorMessage" class="input-response-error-message" :class="{ 'input-response-error-message-show': errorMessage }">{{ errorMessage }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
   import { ref } from "vue";
 
-  const inputValue = ref('');
+  const inputValue = ref<number | null>(null);
+  const errorMessage = ref('');
 
   const updateInput = (event: Event) => {
     const target = event.target as HTMLInputElement;
-    inputValue.value = target.value;
-    emits('update:modelValue', target.value);
+    const input = target.value;
+    const numericInput = input.replace(/\D/g, '');
+    inputValue.value = numericInput === '' ? null : parseInt(numericInput);
+    errorMessage.value = input !== numericInput ? "Que des numéros s'il vous plaît" : '';
+    emits('update:modelValue', inputValue.value);
+    if (errorMessage.value) {
+      setTimeout(() => {
+        errorMessage.value = '';
+      }, 5000);
+    }
   };
 
   const emits = defineEmits<{
-    (e: 'update:modelValue', value: string): void;
+    (e: 'update:modelValue', value: number | null): void;
   }>();
 </script>
 
