8748df9f8fe2088349c7178584fb98a4b7eb3411
KingYano <belkadi.yanis.l@gmail.com>
2024-07-07 03:10:57 +0200

    Mise à jour des dépendances + ajout d'un favicon + modification mineurs de style.

---
+++ src/components/ConfirmExitPopup/ConfirmExitPopup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="confirm-exit-popup">
+    <div class="popup-content">
+      <p>Êtes-vous sûr de vouloir quitter le formulaire ?</p>
+      <div class="popup-buttons">
+        <button @click="stayOnPage">Rester</button>
+        <button @click="exitPage">Quitter</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
+
+  const stayOnPage = () => {
+    // Close the popup
+  };
+
+  const exitPage = () => {
+    router.push('/');
+  };
+</script>
+
+<style lang="scss">
+/* Add your styles here */
+</style>
